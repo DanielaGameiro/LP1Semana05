59b3ff75f48e5f2587cb43e475f7a9ff8bab3639
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-29 15:44:38 +0100

    Solve Tempo exercise

---
+++ Tempo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Elapsed.TotalSeconds
+            // IsRunning
+            // Start (), StartNew(), Reset(), Stop()...
+
+            // Declare and initialize variables
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+            
+            // Start crono1 and crono2
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(300);
+
+            // Stop crono1 and crono2
+            crono1.Stop();
+            crono2.Stop();
+
+            // Show the time
+            Console.WriteLine($"Crono 1: {crono1.Elapsed.TotalSeconds:f2}s");
+            Console.WriteLine($"Crono 2: {crono2.Elapsed.TotalSeconds:f2}s");
+        }
+    }
+}
