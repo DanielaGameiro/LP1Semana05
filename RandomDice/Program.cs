833acfa20a024d435e00986d451d42644237f8a6
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-29 16:41:34 +0100

    Add RandomDice project

---
+++ RandomDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RandomDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, sum = 0;
+            Random random = new Random();
+
+            Console.Write("n = ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int value = random.Next(1, 7);
+                Console.WriteLine($"Para n = {i} -> {value}");
+                sum += value;
+            }
+            
+            Console.WriteLine($"\nA soma será {sum}.");
+        }
+    }
+}
