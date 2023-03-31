bf725c38c6156e36515d6cd6f2f3b7a785ca3d71
Daniela Gameiro <danielagameiro@live.com.pt>
2023-03-31 11:04:57 +0100

    Add UpperUnion project with a ! bug

---
+++ UpperUnion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace UpperUnion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sem argumentos passados
+            if (args.Length == 0)
+            {
+                Console.WriteLine("NO ARGUMENTS");
+            }
+
+            // Com argumentos passados
+            else
+            {
+                // Meter em maiúsculas com ToUpper()
+                for (int i = 0; i < args.Length; i++)
+                {
+                    args[i] = args[i].ToUpper();
+                }
+
+                // Ordenar com Sort()
+                Array.Sort(args);
+
+                // Unir com StringBuilder()
+                StringBuilder finalString = new StringBuilder();
+                finalString.Append(String.Join("-", args));
+
+                Console.WriteLine(finalString);
+            }
+        }
+    }
+}
+
